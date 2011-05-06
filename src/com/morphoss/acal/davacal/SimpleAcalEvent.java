a2fa5578b12dbdb1bb7af532e203112178918f1a
Andrew McMillan <andrew@morphoss.com>
2011-05-07 01:48:11 +1200

    Extend SimpleAcalEvent to be usable for MonthView event list.

--- src/com/morphoss/acal/davacal/SimpleAcalEvent.java
+++ src/com/morphoss/acal/davacal/SimpleAcalEvent.java
@@ -17,7 +17,16 @@
  */
 package com.morphoss.acal.davacal;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+
+import com.morphoss.acal.R;
 import com.morphoss.acal.acaltime.AcalDateTime;
+import com.morphoss.acal.acaltime.AcalDuration;
 
 
 /**
@@ -26,24 +35,102 @@ import com.morphoss.acal.acaltime.AcalDateTime;
  *
  */
 public class SimpleAcalEvent implements Comparable<SimpleAcalEvent> {
+
+	private static final String TAG = "SimpleAcalEvent"; 
+
 	//Start and end times are in UTC
 	public final long start;
 	public final long end;
 	public final long resourceId;
 	public final String summary;
 	public final int colour;
+	public final String location;
+	public final boolean hasRepeat;
+	public final boolean hasAlarm;
+	public final boolean isAllDay;
+	public final boolean isPending;
 	
-	public SimpleAcalEvent(long start, long end, long resourceId, String summary, int colour) {
+	private SimpleAcalEvent(long start, long end, long resourceId, String summary, String location, int colour,
+				boolean isAlarming, boolean isRepetitive, boolean allDayEvent, boolean isPending ) {
 		long st = start;
 		long en = end;
-//		st+= (TimeZone.getDefault().getRawOffset()/1000f);
-//		en+= (TimeZone.getDefault().getRawOffset()/1000f);		//make sure we store time in 'floating time'
 		this.start = st;
 		this.end = en;
 		this.resourceId = resourceId;
 		this.summary = summary;
+		this.location = location;
 		this.colour = colour;
+		this.hasAlarm = isAlarming;
+		this.hasRepeat = isRepetitive;
+		this.isAllDay = allDayEvent;
+		this.isPending = isPending;
 	}
+
+	
+	/**
+	 * Construct a SimpleAcalEvent from bits of VComponent, with overrides for
+	 * startDate & duration so we can build it inside a repeat rule calculation 
+	 * @param event The parsed VComponent which is the event
+	 * @param startDate To override the startDate in the event.
+	 * @param duration To override the duration or end date in the event 
+	 */
+	public SimpleAcalEvent(VComponent event, AcalDateTime startDate, AcalDuration duration, boolean isPending ) {
+		this.resourceId = event.getResourceId();
+		
+		this.isPending = isPending;
+
+		boolean allDayEvent = startDate.isDate();
+		boolean floating = startDate.isFloating();
+		start = startDate.applyLocalTimeZone().getEpoch();
+
+		
+		long en = start - 1; // illegal value to test for...
+		if ( duration != null ) {
+			en = duration.getEndDate(startDate).getEpoch();
+			if ( floating && !allDayEvent && duration.getTimeMillis() == 0 && duration.getDays() > 0)
+				allDayEvent = true;
+		}
+		isAllDay = allDayEvent;
+		
+		if ( en < start ) {
+			AcalProperty dtend = event.getProperty("DTEND");  
+			if (dtend != null)
+				en = AcalDateTime.fromAcalProperty(dtend).applyLocalTimeZone().getEpoch();
+		}
+		if ( en < start ) {
+			if ( startDate.isDate() )
+				en = start + AcalDateTime.SECONDS_IN_DAY;
+			else
+				en = start;
+		}
+		end = en;
+
+		summary = event.safePropertyValue("SUMMARY");
+		location = event.safePropertyValue("LOCATION");
+		String repeats = event.safePropertyValue("RRULE") + event.safePropertyValue("RDATE");
+		hasRepeat = repeats.length()>0;
+
+		boolean isAlarming = false;
+		for( VComponent child : event.getChildren() ) {
+			if ( child instanceof VAlarm ) {
+				isAlarming = true;
+				break;
+			}
+		}
+		hasAlarm = isAlarming;
+		
+		int aColor = Color.BLUE;
+		try {
+			aColor = event.getCollectionColour();
+		} catch (Exception e) {
+			Log.e(TAG,"Error Creating UnModifiableAcalEvent - "+e.getMessage());
+			Log.e(TAG,Log.getStackTraceString(e));
+		}
+		colour = aColor;
+		
+	}
+
+	
 	
 	/**
 	 * Factory method to generate a SimpleAcalEvent from a real AcalEvent.  Since we don't have to worry
@@ -55,9 +142,14 @@ public class SimpleAcalEvent implements Comparable<SimpleAcalEvent> {
 	 */
 	public static SimpleAcalEvent getSimpleEvent(AcalEvent event) {
 		AcalDateTime start = event.getStart(); 
+		boolean allDayEvent = start.isDate() ||	(start.isFloating()
+														&& event.getDuration().getTimeMillis() == 0
+																	&& event.getDuration().getDays() > 0);
 		start.applyLocalTimeZone();
-		return new SimpleAcalEvent(start.getEpoch(), start.getEpoch()+(event.getDuration().getDurationMillis()/1000),
-					event.resourceId, event.summary, event.colour);
+		return new SimpleAcalEvent(start.getEpoch(), event.getDuration().getEndDate(start).getEpoch(),
+					event.resourceId, event.summary, event.location, event.colour,
+					event.hasAlarms(), event.getRepetition().length() > 0, allDayEvent,
+					event.isPending);
 	}
 
 	/**
@@ -106,4 +198,47 @@ public class SimpleAcalEvent implements Comparable<SimpleAcalEvent> {
 	public int compareTo(SimpleAcalEvent seo) {
 		return (int)(this.end - this.start);
 	}
+
+	/**
+	 * Return a pretty string indicating the time period of the event.  If the start or end
+	 * are on a different date to the view start/end then we also include the date on that
+	 * element.  If it is an all day event, we say so. 
+	 * @param viewDateStart - the start of the viewed range
+	 * @param viewDateEnd - the end of the viewed range
+	 * @param as24HourTime - from the pref
+	 * @return A nicely formatted string explaining the start/end of the event.
+	 */
+	public String getTimeText(Context c, long viewDateStart, long viewDateEnd, boolean as24HourTime ) {
+		String timeText = "";
+		String timeFormatString = (as24HourTime ? "HH:mm" : "hh:mmaa");
+		SimpleDateFormat timeFormatter = new SimpleDateFormat(timeFormatString);
+
+		Date st = new Date(start*1000);
+		Date en = new Date(end*1000);
+		if ( start < viewDateStart || end  > viewDateEnd ){
+			if ( isAllDay ) {
+				timeFormatter = new SimpleDateFormat("MMM d");
+				timeText = c.getString(R.string.AllDaysInPeriod, timeFormatter.format(st), timeFormatter.format(en));
+			}
+			else {
+				SimpleDateFormat startFormatter = timeFormatter;
+				SimpleDateFormat finishFormatter = timeFormatter;
+				
+				if ( start < viewDateStart )
+					startFormatter  = new SimpleDateFormat("MMM d, "+timeFormatString);
+				if ( end >= viewDateEnd )
+					finishFormatter = new SimpleDateFormat("MMM d, "+timeFormatString);
+		
+				timeText = startFormatter.format(st)+" - " + finishFormatter.format(en);
+			}
+		}
+		else if ( isAllDay ) {
+			timeText = c.getString(R.string.ForTheWholeDay);
+		}
+		else {
+			timeText = timeFormatter.format(st) + " - " + timeFormatter.format(en);
+		}
+		return timeText;
+	}
+
 }
