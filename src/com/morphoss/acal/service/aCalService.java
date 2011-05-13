7921bf9ff98cf4e9d185a83358cde72c96d53fb2
Andrew McMillan <andrew@morphoss.com>
2011-05-13 20:43:03 +1200

    Start the sync service at a low priority.

--- src/com/morphoss/acal/service/aCalService.java
+++ src/com/morphoss/acal/service/aCalService.java
@@ -23,6 +23,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.IBinder;
+import android.os.Process;
 import android.os.RemoteException;
 import android.util.Log;
 
@@ -43,6 +44,7 @@ public class aCalService extends Service {
 	
 	public void onCreate() {
 		super.onCreate();
+		Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
 		serviceContext = this;
 		startService();
 	}
@@ -87,6 +89,7 @@ public class aCalService extends Service {
 		if (Constants.LOG_DEBUG) Log.d(TAG, "Worker killed.");
 	}
 	
+
 
 	//@Override
 	public IBinder onBind(Intent arg0) {
