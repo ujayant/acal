06383b30a4090d1121d43bb3046d3ad374440799
kris kechagia <kk@rndsec.net>
2011-09-07 00:50:21 +0200

    added aidl files to Android.mk

--- Android.mk
+++ Android.mk
@@ -4,6 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += \
+	src/com/morphoss/acal/dataservice/DataRequestCallBack.aidl \
+	src/com/morphoss/acal/dataservice/DataRequest.aidl \
+	src/com/morphoss/acal/service/ServiceRequest.aidl \
 
 LOCAL_PACKAGE_NAME := Acal
 LOCAL_CERTIFICATE := platform
