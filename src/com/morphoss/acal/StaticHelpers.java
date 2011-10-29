1ff2ec366c326521e1e321776614d0a35d2dad0f
Andrew McMillan <andrew@morphoss.com>
2011-10-29 22:37:55 +1300

    New methods to setContainerColour() and pickForegroundForBackground()
    Used for the nascent theming stuff.

--- src/com/morphoss/acal/StaticHelpers.java
+++ src/com/morphoss/acal/StaticHelpers.java
@@ -22,7 +22,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.content.ContentValues;
+import android.graphics.Color;
 import android.os.Parcel;
+import android.view.View;
+import android.view.ViewParent;
 
 
 public final class StaticHelpers {
@@ -200,5 +203,40 @@ public final class StaticHelpers {
 		return src.readLong();
 	}
 	
-	
+
+	/**
+	 * The way we are theming some things is to have a LinearLayout with a solid
+	 * background assigned, containing (normally) a button with a 9-patch which is
+	 * white/transparent overlay.  This utility helps us set the colour on the
+	 * relevant object.
+	 * 
+	 * @param someView
+	 * @param someColour
+	 */
+	public static void setContainerColour(View someView, int someColour) {
+		ViewParent vp;
+		do {
+			vp = someView.getParent();
+		} while ( !(vp instanceof View) );
+		((View) vp).setBackgroundColor(someColour);
+	}
+
+
+	/**
+	 * Given the supplied background colour, tries to pick a foreground colour with good
+	 * contrast which is not too jarring against it.
+	 * 
+	 * This initial implementation is simplistic and probably needs refinement.
+	 * 
+	 * @param backgroundColour
+	 * @return
+	 */
+	public static int pickForegroundForBackground( int backgroundColour ) {
+		int r = (backgroundColour & 0xff0000) >> 32;
+		int g = (backgroundColour & 0xff00) >> 16;
+		int b = (backgroundColour & 0xff);
+		
+		if ( ((r + g + b) / 3) < 150 ) return Color.WHITE;
+		return Color.BLACK;
+	}
 }
