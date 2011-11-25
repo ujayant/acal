fe6b63e5158a3f3fc203c53f29aefc7443087f66
Andrew McMillan <andrew@morphoss.com>
2011-11-25 20:37:24 +1300

    Constants as they should be.

--- src/com/morphoss/acal/Constants.java
+++ src/com/morphoss/acal/Constants.java
@@ -50,26 +50,30 @@ public class Constants {
 	public static final boolean DEBUG_MODE = false;
 
 	/** How much stuff to spit out into the logs */
-	public static final boolean LOG_VERBOSE = true & DEBUG_MODE;		//Very verbose play by play execution information
-	public static final boolean LOG_DEBUG = true & DEBUG_MODE;			//Information relevant to debugging tasks.
-	public static final boolean DEBUG_SETTINGS = true & DEBUG_MODE;	// Does the debugging menu appear in Settings
+	public static final boolean LOG_VERBOSE = false && DEBUG_MODE;		//Very verbose play by play execution information
+	public static final boolean LOG_DEBUG = false && DEBUG_MODE;			//Information relevant to debugging tasks.
+	public static final boolean DEBUG_SETTINGS = false && DEBUG_MODE;	// Does the debugging menu appear in Settings
 
 	/** Since Andrew's device won't display logs at DEBUG level he needs a way to fake that! */
-	public static final int LOGV = Log.INFO;  // Normally should be Log.VERBOSE of course.
-	public static final int LOGD = Log.INFO;  // Normally should be Log.DEBUG of course.
+	public static final int LOGV = Log.VERBOSE;  // Normally should be Log.VERBOSE of course.
+	public static final int LOGD = Log.DEBUG;  // Normally should be Log.DEBUG of course.
+	public static final int LOGI = Log.INFO;  // Normally should be Log.INFO of course.
+	public static final int LOGW = Log.WARN;  // Normally should be Log.WARN of course.
+	public static final int LOGE = Log.ERROR; // Normally should be Log.ERROR of course.
 	
 	/** And sometimes we want to really deeply debug specific bits */
-	public static final boolean	debugRepeatRule					= false & DEBUG_MODE;
-	public static final boolean	debugCalendar					= false & DEBUG_MODE;
-	public static final boolean	debugSyncCollectionContents		= true & DEBUG_MODE;
-	public static final boolean	debugCalendarDataService		= false & DEBUG_MODE;
-	public static final boolean	debugMonthView					= false & DEBUG_MODE;
-	public static final boolean	debugWeekView					= false & DEBUG_MODE;
-	public static final boolean	debugVComponent					= false & DEBUG_MODE;
-	public static final boolean	debugDateTime					= false & DEBUG_MODE;
-	public static final boolean	debugDavCommunication			= false & DEBUG_MODE;
-	public static final boolean	debugAlarms						= false & DEBUG_MODE;
-	public static final boolean	debugHeap						= false & DEBUG_MODE;
+	public static final boolean	debugRepeatRule					= false && DEBUG_MODE;
+	public static final boolean	debugCalendar					= false && DEBUG_MODE;
+	public static final boolean	debugSyncCollectionContents		= false && DEBUG_MODE;
+	public static final boolean	debugCalendarDataService		= false && DEBUG_MODE;
+	public static final boolean	debugMonthView					= false && DEBUG_MODE;
+	public static final boolean	debugWeekView					= false && DEBUG_MODE;
+	public static final boolean	debugVComponent					= false && DEBUG_MODE;
+	public static final boolean	debugDateTime					= false && DEBUG_MODE;
+	public static final boolean	debugDavCommunication			= false && DEBUG_MODE;
+	public static final boolean	debugAlarms						= false && DEBUG_MODE;
+	public static final boolean	debugHeap						= false && DEBUG_MODE;
+	public static final boolean debugCheckServerDialog 			= false && DEBUG_MODE;
 	
 	public static final long DEFAULT_MAX_AGE_WIFI = 1000*60*30;		// The default to use when initialising a new collection
 	public static final long DEFAULT_MAX_AGE_3G = 1000*60*60*2;		// The default to use when initialising a new collection
