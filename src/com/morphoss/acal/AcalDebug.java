f98d7d0381f25f74715da575a8c1ef24d7938442
Andrew McMillan <andrew@morphoss.com>
2011-11-25 21:46:00 +1300

    Add a little more (debug) info to the memory status.

--- src/com/morphoss/acal/AcalDebug.java
+++ src/com/morphoss/acal/AcalDebug.java
@@ -18,6 +18,8 @@
 
 package com.morphoss.acal;
 
+import android.app.ActivityManager;
+import android.os.Debug;
 import android.util.Log;
 
 
@@ -35,5 +37,22 @@ public final class AcalDebug {
 		used /= 1024;
 		int logLevel = ( percentUsed > 80 ? Log.ERROR : (percentUsed > 50 ? Log.WARN : Log.INFO) );
 		Log.println(logLevel, TAG, String.format("%-50.50s: Heap used: %dk (%.2f%%) of max: %dk", msg, used, percentUsed, r.maxMemory()/1024));
+
+		Debug.MemoryInfo mi = new Debug.MemoryInfo();
+		try {
+			Debug.getMemoryInfo(mi);
+			if ( mi != null ) {
+				Log.println(Constants.LOGD,TAG,String.format("MemoryInfo: Dalvik(%d,%d,%d), Native(%d,%d,%d), Other(%d,%d,%d)",
+							mi.dalvikPrivateDirty, mi.dalvikPss, mi.dalvikSharedDirty,
+							mi.nativePrivateDirty, mi.nativePss, mi.nativeSharedDirty,
+							mi.otherPrivateDirty,  mi.otherPss,  mi.otherSharedDirty ) );
+			}
+			ActivityManager.MemoryInfo ammi = new ActivityManager.MemoryInfo();
+			if ( ammi.lowMemory )
+				Log.w(TAG, "Android thinks we're low memory right now, rescheduling more sync in 30 seconds time." );
+		}
+		catch ( Exception e ) {
+			Log.i(TAG,"Unable to get Debug.MemoryInfo() because: " + e.getMessage());
+		}
 	}
 }
