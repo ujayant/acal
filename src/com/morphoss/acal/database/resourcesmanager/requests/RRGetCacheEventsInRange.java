63113f4a28a235851633e803c148f8eb88f1d7ff
Andrew McMillan <andrew@morphoss.com>
2011-12-17 23:08:40 +1300

    Adjust query to better include VTDODO in the response.

--- src/com/morphoss/acal/database/resourcesmanager/requests/RRGetCacheEventsInRange.java
+++ src/com/morphoss/acal/database/resourcesmanager/requests/RRGetCacheEventsInRange.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 import android.content.ContentValues;
 import android.util.Log;
 
+import com.morphoss.acal.Constants;
 import com.morphoss.acal.acaltime.AcalDateRange;
 import com.morphoss.acal.database.cachemanager.CacheObject;
-import com.morphoss.acal.database.resourcesmanager.ResourceResponse;
-import com.morphoss.acal.database.resourcesmanager.ResourceResponseListener;
 import com.morphoss.acal.database.resourcesmanager.ResourceManager.ReadOnlyResourceTableManager;
 import com.morphoss.acal.database.resourcesmanager.ResourceManager.ResourceTableManager;
+import com.morphoss.acal.database.resourcesmanager.ResourceResponse;
+import com.morphoss.acal.database.resourcesmanager.ResourceResponseListener;
 import com.morphoss.acal.database.resourcesmanager.requesttypes.ReadOnlyResourceRequestWithResponse;
 import com.morphoss.acal.dataservice.Resource;
 import com.morphoss.acal.davacal.VCalendar;
@@ -26,32 +27,32 @@ public class RRGetCacheEventsInRange extends ReadOnlyResourceRequestWithResponse
 	
 	public RRGetCacheEventsInRange(AcalDateRange range, ResourceResponseListener<ArrayList<CacheObject>> callback) {
 		super(callback);
-		Log.d(TAG,"Instatiated");
+		Log.println(Constants.LOGD,TAG,"Instantiated for range of "+range.toString());
 		this.range = range;
 	}
 	
 	@Override
 	public void process(ReadOnlyResourceTableManager processor) {
 		// TODO At the moment this algorithm ignores pending resources nor does it handle floating events.
-		Log.d(TAG,"Process Called...");
+		Log.println(Constants.LOGD,TAG,"Process Called...");
 		
 		//step 1 --> convert range to long UTC start 
 		long start = range.start.getMillis();
 		long end = range.end.getMillis();
 		
-		Log.d(TAG,"Getting Resource rows");
+		Log.println(Constants.LOGD,TAG,"Getting Resource rows");
 		//step 2 query db for reesources in range
-		ArrayList<ContentValues> rValues = processor.query(null, 
-				
-				"( "+ResourceTableManager.LATEST_END+" ISNULL OR " + ResourceTableManager.LATEST_END+" >= ? )" +
-				" AND "+ResourceTableManager.EARLIEST_START+" <= ? "
+		ArrayList<ContentValues> rValues = processor.query(null,
+				"("+ResourceTableManager.EFFECTIVE_TYPE +"=? OR "+ResourceTableManager.EFFECTIVE_TYPE +"=? )" +
+				" AND ("+ResourceTableManager.LATEST_END+" IS NULL OR " + ResourceTableManager.LATEST_END+" >= ? )" +
+				" AND ("+ResourceTableManager.EARLIEST_START+" IS NULL OR "+ResourceTableManager.EARLIEST_START+" <= ? )"
 				,
-				new String[]{  start+"", end+""},
+				new String[]{ VComponent.VEVENT, VComponent.VTODO, start+"", end+""},
 				null,null,null);
-		Log.d(TAG,rValues.size()+" Rows retreived. Converting into Resource Objects");
+		Log.println(Constants.LOGD,TAG,rValues.size()+" Rows retreived. Converting into Resource Objects");
 		ArrayList<Resource> resources = new ArrayList<Resource>();
 		for (ContentValues cv : rValues) resources.add(Resource.fromContentValues(cv));
-		Log.d(TAG, "Conversion complete. Populating VCalendars and appedning events.");
+		Log.println(Constants.LOGD,TAG, "Conversion complete. Populating VCalendars and appedning events.");
 		ArrayList<CacheObject> events = new ArrayList<CacheObject>();
 		//step 3 - foreach resource, Vcomps
 		//This is very CPU intensive, so lower our priority to prevent interfering with other parts of the app.
@@ -65,12 +66,11 @@ public class RRGetCacheEventsInRange extends ReadOnlyResourceRequestWithResponse
 					((VCalendar)comp).appendCacheEventInstancesBetween(events, range, false);
 				}
 			} catch (VComponentCreationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				Log.i(TAG,Log.getStackTraceString(e));
 			}
 		}
 		Thread.currentThread().setPriority(currentPri);
-		Log.d(TAG,events.size()+"Event Instances obtained. Posting Response.");
+		Log.println(Constants.LOGD,TAG,events.size()+"Event Instances obtained. Posting Response.");
 			
 		//post response
 		super.postResponse(new RREventsInRangeResponse<ArrayList<CacheObject>>(events, range));
