310fcf31abe7a26bf5a43888ca06b9bd3665942b
Chris Noldus <chris@morphoss.com>
2011-12-21 09:39:42 +1300

    Same as previous

---
+++ src/com/morphoss/acal/database/DMQueryList.java
@@ -0,0 +1,16 @@
+package com.morphoss.acal.database;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DMQueryList {
+	private ArrayList<DMAction> actions = new ArrayList<DMAction>();
+	public void addAction(DMAction action) { actions.add(action); }
+	
+	public List<DMAction> getActions() { return Collections.unmodifiableList(actions); }
+	
+	public boolean isEmpty() {
+		return actions.isEmpty();
+	}
+}
