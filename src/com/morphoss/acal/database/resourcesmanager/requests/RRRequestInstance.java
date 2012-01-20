9ae6a96ef1c991abeb6e9d28f9a881035f46ea59
Andrew McMillan <andrew@morphoss.com>
2012-01-21 12:52:56 +1300

    Formatting.

--- src/com/morphoss/acal/database/resourcesmanager/requests/RRRequestInstance.java
+++ src/com/morphoss/acal/database/resourcesmanager/requests/RRRequestInstance.java
@@ -41,16 +41,16 @@ public class RRRequestInstance extends ReadOnlyResourceRequestWithResponse<Calen
 		ArrayList<ContentValues> cv = processor.query(null, ResourceTableManager.RESOURCE_ID+" = ?", new String[]{rid+""}, null,null,null);
 		ArrayList<ContentValues> pcv = processor.getPendingResources();
 		try {
-			
 			//check pending first
 			for (ContentValues val : pcv) {
 				if (val.getAsLong(ResourceTableManager.PEND_RESOURCE_ID) == this.rid) {
 					String blob = val.getAsString(ResourceTableManager.NEW_DATA);
-					if (blob == null || blob.equals("")) {
-						//this resource has been deleted
+					if ( blob == null || blob.equals("") ) {
+						// this resource has been deleted
 						throw new Exception("Resource deleted.");
-					} else {
-						CalendarInstance ci = CalendarInstance.fromPendingRowAndRRID(val,rrid);
+					}
+					else {
+						CalendarInstance ci = CalendarInstance.fromPendingRowAndRRID(val, rrid);
 						this.postResponse(new RRRequestInstanceResponse(ci));
 						this.setProcessed();
 					}
@@ -76,7 +76,8 @@ public class RRRequestInstance extends ReadOnlyResourceRequestWithResponse<Calen
 		private CalendarInstance result = null;
 		public RRRequestInstanceResponse(CalendarInstance ci) {
 			this.result = ci;
-			if ( Constants.LOG_DEBUG ) Log.println(Constants.LOGD, TAG, "Resource "+ci.getResourceId()+", recurrence "+ci.getRecurrenceId()+" is returned.");
+			if ( Constants.LOG_DEBUG ) Log.println(Constants.LOGD, TAG,
+					"Resource "+ci.getResourceId()+", recurrence "+ci.getRecurrenceId()+" is returned.");
 		}
 		public RRRequestInstanceResponse(Exception e) { super(e); }
 		
