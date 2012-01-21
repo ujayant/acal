6a6a856d057cb6f1b7d33ea24bc012e10c8bc56b
Andrew McMillan <andrew@morphoss.com>
2012-01-21 22:05:49 +1300

    Also catch IOException.

--- src/com/morphoss/acal/service/connector/AcalRequestor.java
+++ src/com/morphoss/acal/service/connector/AcalRequestor.java
@@ -20,6 +20,7 @@ package com.morphoss.acal.service.connector;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
@@ -41,7 +42,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.conn.ConnectionPoolTimeoutException;
-import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
@@ -773,6 +773,10 @@ public class AcalRequestor {
 			return null;
 		}
 		catch ( UnknownHostException e ) {
+			Log.i(TAG, e.getClass().getSimpleName() + ": " + e.getMessage() + " to " + fullUrl() );
+			return null;
+		}
+		catch ( IOException e ) {
 			Log.i(TAG, e.getClass().getSimpleName() + ": " + e.getMessage() + " to " + fullUrl() );
 			return null;
 		}
