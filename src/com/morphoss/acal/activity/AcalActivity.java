23f9be77f08ca4d27eaf7c539876ef04eb99710e
Andrew McMillan <andrew@morphoss.com>
2012-08-19 00:34:37 +1200

    Initialise theme during activity create.  Seems to late in resume.

--- src/com/morphoss/acal/activity/AcalActivity.java
+++ src/com/morphoss/acal/activity/AcalActivity.java
@@ -14,6 +14,7 @@ public abstract class AcalActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		AcalTheme.initializeTheme(this);
 	}	
 
 	@Override
